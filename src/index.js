0674f7a2ef78c2292e3a3bd67257ed9af8d46c89
Alexander Ashwood <alexander.ashwood@sky.uk>
2025-08-06 10:29:00 +0100

    feat: update let agreed styles

--- src/index.js
+++ src/index.js
@@ -14,9 +14,20 @@ const loadProperties = () => {
         propertyElement.className = 'property';
         propertyElement.innerHTML = `
                     <a href="#" class=rounded-3xl>
-                        <img class="rounded-3xl flex-1 object-cover mb-2" src="${property.imageUrl}" alt="${property.title}" class="property-image" />  
+                        <div class="relative">
+                        <img class="rounded-3xl flex-1 object-cover mb-2" src="${
+                          property.imageUrl
+                        }" alt="${property.title}" class="property-image" />
+                        ${
+                          property.letAgreed
+                            ? '<div class="absolute top-0 left-0 w-full h-full bg-white opacity-65"></div><p class="absolute right-2 top-2 text-white text-sm bg-blue-400 px-2 py-1 rounded-3xl">Let Agreed</p>'
+                            : ''
+                        }
+                        </div>
                         <h2 class="truncate">${property.title}</h2>
-                        <p class="text-neutral-400 text-sm truncate">${property.description}</p>
+                        <p class="text-neutral-400 text-sm truncate">${
+                          property.description
+                        }</p>
                         <p class="underline mt-2">£${property.rentPerMonth}</p>
                         <p class="text-neutral-400">per month</p>
                     </a>
